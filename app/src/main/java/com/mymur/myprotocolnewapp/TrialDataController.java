ea21199878aed2997ab2b0c695f22bc95ba37994
AnastasiaDro <adrogunova@gmail.com>
2020-03-01 10:31:32 +0300

    Сделано: -начала делать TrialDataController
    Сделать
    - обработчик кнопок для TrialDataFragment

---
+++ app/src/main/java/com/mymur/myprotocolnewapp/TrialDataController.java
@@ -0,0 +1,12 @@
+package com.mymur.myprotocolnewapp;
+
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class TrialDataController {
+    Button saveResultBtn;
+    RadioGroup physicalHints;
+
+
+}
